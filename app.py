b3a717be9c65f78bccbc3bb71fbd8268055d86a2
Sid D <smartsid2007@gmail.com>
2023-10-07 01:38:44 +0000

    Minor enhancement

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
-
+import numpy as np
 from lang_utils import ask_to_all_pdfs_sources, create_qa_retrievals, get_text_splitter
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 # SETUP ------------------------------------------------------------------------
 #favicon = Image.open("/home/obiraj77/star/favicon.ico")
@@ -72,104 +78,96 @@ Title_html = """
     """
 components.html(Title_html)
 
-with st.form("basic_form"):
+uploaded_files = st.file_uploader(
+    "Upload files",
+    type=["pdf"],
+    key="file_upload_widget",
+    accept_multiple_files=True,
+)
 
-    # openai_api_key = st.text_input("OpenAI API Key", key="openai_api_key")
+if uploaded_files is not None and len(uploaded_files) > 0:
+    st.session_state.processing_button_clicked = False
 
-    uploaded_files = st.file_uploader(
-        "Upload files",
-        type=["pdf"],
-        key="file_upload_widget",
-        accept_multiple_files=True,
-    )
+    if True: #st.button("Start Processing") or st.session_state.processing_button_clicked == True:
+        st.session_state.file_name = uploaded_files[0]
+        st.session_state.processing_button_clicked = True
 
-    # question_1 = st.text_input("Question 1", key="1_question")
-    # question_2 = st.text_input("Question 2", key="2_question")
-    # question_3 = st.text_input("Question 3", key="3_question")
-    # question_4 = st.text_input("Question 4", key="4_question")
-    # question_5 = st.text_input("Question 5", key="5_question")
-
-    submit_btn = st.form_submit_button("Start Processing")
-
-    if submit_btn:
-        # if openai_api_key == "":
-        #     st.warning("You need an API key from OpenAI to use thise App")
-        #     st.stop()
-
-        # if question_1 == "":
-        #     st.warning("Give at least one question")
-        #     st.stop()
-
-        if uploaded_files is None or len(uploaded_files) != 2:
-            st.warning("Upload exactly 2 PDf files")
+        if uploaded_files is None or len(uploaded_files) != 1:
+            st.warning("Upload exactly 1 PDf files")
             st.stop()
-        # all_questions = [
-        #     question_1,
-        #     question_2,
-        #     question_3,
-        #     question_4,
-        # ]  # , question_5]
 
-        # for pdf in uploaded_files:
-        #     # st.info(f"Processing {pdf.name}")
         srcText = get_text_splitter(uploaded_files[0])
-        destText = get_text_splitter(uploaded_files[1])
 
-        
-        with st.spinner("Creating embeddings...."):
-            try:
-                a = 1
-                # st.session_state.qa_retrievals = create_qa_retrievals(
-                #     uploaded_files, openai_api_key
-                # )
-                # st.session_state.questions = all_questions
-            except Exception as e:
+        try:
+            st.write(
+                    "LIST OF SUGGESTIONS. PLEASE SELECT AND ACCEPT"
+                )
 
-                st.error("Something went grong...")
-                st.exception(e)
-                st.stop()
-        st.success("Processing Files!", icon="✅")
-        with st.spinner("Doing Analysis...."):
+            # Cache the dataframe so it's only loaded once
+            # @st.cache_data
+            def load_data():
+                return pd.DataFrame(
+                    [
+                        {"beforText":"VASUKI DIVYA PONDURI", "afterText": "Sid Darapuram", "apply_Selection": True},
+                        { "beforText":"vasukidivya006@gmail.com", "afterText": "smartsid2007@gmail.com", "apply_Selection": False},
+                        { "beforText":"Sample original text", "afterText": "Revised New Text", "apply_Selection": True},
+                    ]
+                )
 
-            try:
-                data = []
-                # for question in st.session_state.questions:
-                #     if question == "":
-                #         continue
-                #     results = ask_to_all_pdfs_sources(
-                #         question, st.session_state.qa_retrievals
-                #     )
-                # data.extend(texts)
-                # st.session_state.data = data
+            df = load_data()
+            edited_df = st.data_editor(df) # 👈 An editable dataframe
 
-            except Exception as e:
+        except Exception as e:
 
-                st.error("Something went grong...")
-                st.exception(e)
-                st.stop()
-        st.success("Analyzing Files!", icon="✅")
-        with st.spinner("Doing Analysis.."):
-            try:
-                # df = pd.DataFrame(st.session_state.data)
-                # st.table(
-                #     df.pivot(
-                #         index="query", columns="source_document", values="response"
-                #     )
-                # )
-                
-                col1, col2 = st.columns(2)
-                col1.header("Original")
-                col1.write(srcText)
-
-                col2.header("Revolutionized")
-                col2.write(destText)
-
-            except Exception as e:
-
-                st.error("Something went grong...")
-                st.exception(e)
-                st.stop()
+            st.error("Something went grong...")
+            st.exception(e)
+            st.stop()
 
 
-            st.snow()
+        #st.snow()
+        edited_df = edited_df[edited_df["apply_Selection"] ==True]
+
+        st.table(edited_df)
+
+
+
+########################### END OF CODE ################################
+
+        # for i in range(len(edited_df)):
+        #     #if(edited_df.loc[i, "apply_Selection"] == True):
+        #     print(edited_df.loc[i, "beforText"], edited_df.loc[i, "afterText"], edited_df.loc[i, "apply_Selection"])
+        # row0_toggle = edited_df.loc[0, "apply_Selection"]
+        # st.markdown(f"Row 0 is  **{row0_toggle}** 🎈")
+        # row1_toggle = edited_df.loc[1, "apply_Selection"]
+        # st.markdown(f"Row 1 is  **{row1_toggle}** 🎈")
+    # st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+        # st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+
+        # df2 = pd.DataFrame(
+        #     [
+        #         {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        #         {"command": "st.balloons", "rating": 5, "is_widget": False},
+        #         {"command": "st.time_input", "rating": 3, "is_widget": True},
+        #     ]
+        # )
+        # edited_df2 = st.data_editor(df2)
+
+        # favorite_command = edited_df2.loc[edited_df2["rating"].idxmax()]["command"]
+        # st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+        # confirm_changes_btn = st.form_submit_button("Confirm Changes")
+        # if confirm_changes_btn:
+        #     for i in range(len(edited_df)):
+        #         if(edited_df.loc[i, "apply_Selection"] == True):
+        #             print(edited_df.loc[i, "beforText"], edited_df.loc[i, "afterText"], edited_df.loc[i, "apply_Selection"])
+        #         # else:
+        #         #     edited_df = data.drop(0)
+        #     edited_df = edited_df.loc[edited_df["apply_Selection"] ==True]
+        #     st.table(edited_df)
+
+   
+
+
+
 
